a74d73d65fc936398195c495f94154b8000bbdb2
Imaraika <inange2013@gmail.com>
2019-08-25 18:11:22 +0200

    component and class have been created

---
+++ src/app/quote-comp/quote-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-comp',
+  templateUrl: './quote-comp.component.html',
+  styleUrls: ['./quote-comp.component.css']
+})
+export class QuoteCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/quote.ts
@@ -0,0 +1,7 @@
+export class Quote {
+    showDescription:boolean
+    constructor(public id: number,public authorName: string,public firstName: string,public quote:string){
+   this.showDescription=false;
+}
+
+}
