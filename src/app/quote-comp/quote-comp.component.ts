9cff3bbc91a2a322fa54f71384a4ed85a851942d
Imaraika <inange2013@gmail.com>
2019-08-25 18:17:20 +0200

    component and class have been created

--- src/app/quote-comp/quote-comp.component.ts
+++ src/app/quote-comp/quote-comp.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import{Quote} from '../quote';
+
 
 @Component({
   selector: 'app-quote-comp',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-comp.component.css']
 })
 export class QuoteCompComponent implements OnInit {
-
+  toggleDetails(index){
+    this.manyQuotes[index].showDescription = !this.manyQuotes[index].showDescription;
+  }
+  completeQuote(isComplete, index){
+    if (isComplete) {
+      this.manyQuotes.splice(index,1);
+    }
+  }
+  manyQuotes:Quote[] =[
+    {id:2,authorName:"Chris Brady",firstName:"Ange",quote:"No guts, no story",showDescription:false},
+  ];
+  addNewQuote(quote){
+    // let goalLength = this.manyQuotes.length;
+    // goal.id = goalLength+1;
+    // goal.completeDate = new Date(goal.completeDate)
+    this.manyQuotes.push(quote)
+  }
   constructor() { }
 
   ngOnInit() {
