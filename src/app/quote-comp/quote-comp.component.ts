761b86aa1a8a9d803483d2927851609b7ae32f11
Imaraika <inange2013@gmail.com>
2019-08-25 19:42:43 +0200

    changes

--- src/app/quote-comp/quote-comp.component.ts
+++ src/app/quote-comp/quote-comp.component.ts
@@ -17,13 +17,25 @@ export class QuoteCompComponent implements OnInit {
     }
   }
   manyQuotes:Quote[] =[
-    {id:2,authorName:"Chris Brady",firstName:"Ange",quote:"No guts, no story",showDescription:false},
+    {id:1,authorName:"Chris Brady",firstName:"Ange",quote:"No guts, no story",showDescription:false},
+    {id:2,authorName:"Chris Brady",firstName:"Ange",quote:"My life is my message",showDescription:false},
+    {id:3,authorName:"Chris Brady",firstName:"Ange",quote:"Screw it, let’s do it",showDescription:false},
   ];
   addNewQuote(quote){
     // let goalLength = this.manyQuotes.length;
     // goal.id = goalLength+1;
     // goal.completeDate = new Date(goal.completeDate)
     this.manyQuotes.push(quote)
+  }
+  upvote = 0;
+  downvote = 0;
+
+  upVoteQuote(){
+   
+  this.upvote++ ; 
+  }
+  downVoteQuote(){
+    this.downvote++;
   }
   constructor() { }
 
